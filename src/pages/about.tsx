2f0ae6ee8df453b03735f3812f45ccbd3cf76c46
Vishal Pathak <vishal.pathak@nagarro.com>
2025-01-16 13:30:51 +0530

    added about page

---
+++ src/pages/about.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Layout from "../components/Layout";
+
+const AboutPage = () => {
+  return (
+    <Layout>
+      <h1>About Us</h1>
+      <p>This is the about page of our store.</p>
+    </Layout>
+  );
+};
+
+export default AboutPage;
